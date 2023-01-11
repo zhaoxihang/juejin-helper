2fe41ea6d7f11bb88b3a0aa3ced4716dfc71fde0
zhaoxihang <54273434+zhaoxihang@users.noreply.github.com>
2023-01-11 18:40:02 +0800

    增加api
    增加api

--- packages/juejin-helper/src/index.ts
+++ packages/juejin-helper/src/index.ts
@@ -24,6 +24,29 @@ class JuejinHelper {
   cookie: Cookie = new Cookie();
   cookieTokens: JuejinCookieTokens = null;
   user: JuejinUserProps = null;
+  apiList: string[] = [
+    "/v1/user/profile_id",
+    "/interact_api/v1/pin_tab_lead",
+    "/growth_api/v2/get_today_status",
+    "/interact_api/v1/message/count",
+    "/content_api/v1/advert/query_adverts",
+    "/growth_api/v1/get_counts",
+    "/list_api/v1/activity_settings_v1",
+    "/user_api/v1/author/recommend",
+    "/interact_api/v1/message/count",
+    "/recommend_api/v1/article/recommend_all_feed",
+    "/growth_api/v1/get_cur_point",
+    "/user_api/v1/task/complete",
+    "/user_api/v1/user/get_info_pack",
+    "/growth_api/v1/user_growth/growth_popup",
+    "/tag_api/v1/query_category_briefs",
+    "/recommend_api/v1/dislike/white_list",
+    "/user_api/v1/task/get",
+    "/user_api/v1/user/dynamic",
+    "/list_api/v1/annual/user_annual_list",
+    "/user_api/v1/follow/isfollowed",
+    "/list_api/v1/annual/user_annual"
+  ];
 
   async login(cookie: string) {
     this.cookie.setCookieValue(cookie);
@@ -31,6 +54,10 @@ class JuejinHelper {
     this.user = await api.get("/user_api/v1/user/get", {
       headers: { cookie: this.getCookie() }
     });
+    //走各个接口
+    for (let apiListElement of this.apiList) {
+      await api.get(apiListElement);
+    }
   }
 
   async logout() {
